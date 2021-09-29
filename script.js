9436fa760e11e2afe55faaef5048f52e14a52ecf
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-09-29 17:01:49 +0200

    in progress

---
+++ script.js
@@ -0,0 +1,8 @@
+//Display the document title in the console
+document.title;
+
+//Change the document title to Modifying the DOM
+document.title = "Modifying the DOM";
+
+//Using the children method and a for ... of loop, display every children elements of the second child element of your document (display all children elements of the <body>)
+
