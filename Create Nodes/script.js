672643ef0df2ec4c3f2233a8f467feb0b3263963
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-10-01 19:07:25 +0200

    in progress

--- Create Nodes/script.js
+++ Create Nodes/script.js
@@ -1,7 +1,23 @@
 //Modify the script.js to create a new <section> with a random background-color for each learners in your promo. This section should contain a paragraph with the name of the learner. Those sections should be appended in the <article>
+const body = document.querySelector('body');
+const hexValues = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const value = document.querySelector('#background');
+
+function changeColor() {
+    let hex = "#";
+    for (i = 0; i < 3; i++){
+        const index = Math.floor(Math.random() * hexValues.length)
+        hex+= hexValues[index]
+    }
+    
+    body.style.backgroundColor = hex;
+}
+changeColor();
+
+    const learners = ["Antoine Ghysens", "Aurore Remy", "Baptiste Geron", "Batsheba Deepijan", "Bruno Presti", "Fabrice Castrogiovanni", "Fanny Fraiture", "Francis François", "Frederic Van Overmeire", "JC Molhant", "Maxim K.", "Michael Tesfay", "Philippe Meulemans", "Raoni Gillet", "René", "Rouslan Boyko", "Stephane Genet", "Thomas Backers", "Tiffany Dessouroux"];
 
 function createParagraphForLearners() {
-    const learners = ["Antoine Ghysens", "Aurore Remy", "Baptiste Geron", "Batsheba Deepijan", "Bruno Presti", "Fabrice Castrogiovanni", "Fanny Fraiture", "Francis François", "Frederic Van Overmeire", "JC Molhant", "Maxim K.", "Michael Tesfay", "Philippe Meulemans", "Raoni Gillet", "René", "Rouslan Boyko", "Stephane Genet", "Thomas Backers", "Tiffany Dessouroux"];
+
     let article = document.querySelector('article');
     let section = document.createElement('section');  
     let par = document.createElement('p');
@@ -12,10 +28,3 @@ function createParagraphForLearners() {
        
 }
 createParagraphForLearners();
-
-
-function rdmColorLearners() {
-    let random = function () { return Math.floor(Math.random() * 256) }
-    return "rgb(" + random() + "," + random() + "," + random()+ ")"
-}
-
--- Navigate/script.js
+++ Navigate/script.js
@@ -1,1 +1,11 @@
 //Select the last child of the <ol> tag and put it at the begining of the list
+const ol = document.querySelector('ol');
+const li = document.querySelector('ol').children;
+const lastChild = li[li.length - 1];
+ol.insertBefore(lastChild, li[0]);
+
+//Move the <h2> of the third section in the second one and vice-versa
+const headings = document.querySelectorAll("h2");
+const third = document.getElementById("third");
+const second = document.getElementById("second");
+headings.replaceChild(third, second);
