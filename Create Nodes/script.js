24bf7919417e1f0f49833dd96fea62d8cf3e9738
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-10-02 19:53:28 +0200

    in progress

--- Create Nodes/script.js
+++ Create Nodes/script.js
@@ -1,30 +1,48 @@
 //Modify the script.js to create a new <section> with a random background-color for each learners in your promo. This section should contain a paragraph with the name of the learner. Those sections should be appended in the <article>
-const body = document.querySelector('body');
-const hexValues = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const value = document.querySelector('#background');
-
-function changeColor() {
-    let hex = "#";
-    for (i = 0; i < 3; i++){
-        const index = Math.floor(Math.random() * hexValues.length)
-        hex+= hexValues[index]
-    }
-    
-    body.style.backgroundColor = hex;
+function randomColorLearners() {
+    let random = function () { return Math.floor(Math.random() * 256) }
+    return "rgb(" +random() + "," + random() + "," + random()+ ")"
 }
-changeColor();
 
-    const learners = ["Antoine Ghysens", "Aurore Remy", "Baptiste Geron", "Batsheba Deepijan", "Bruno Presti", "Fabrice Castrogiovanni", "Fanny Fraiture", "Francis François", "Frederic Van Overmeire", "JC Molhant", "Maxim K.", "Michael Tesfay", "Philippe Meulemans", "Raoni Gillet", "René", "Rouslan Boyko", "Stephane Genet", "Thomas Backers", "Tiffany Dessouroux"];
+const nameOfLearners = ["Antoine Ghysens", "Aurore Remy", "Baptiste Geron", "Batsheba Deepijan", "Bruno Presti", "Fabrice Castrogiovanni", "Fanny Fraiture", "Francis François", "Frederic Van Overmeire", "JC Molhant", "Maxim K.", "Michael Tesfay", "Philippe Meulemans", "Raoni Gillet", "René", "Rouslan Boyko", "Stephane Genet", "Thomas Backers", "Tiffany Dessouroux"];
+let article = document.querySelector('article');
 
-function createParagraphForLearners() {
 
-    let article = document.querySelector('article');
-    let section = document.createElement('section');  
+for (item of nameOfLearners) {
+    let section = document.createElement('section');
+    article.appendChild(section);
+
     let par = document.createElement('p');
-    let text = document.createTextNode(learners);
-    par.appendChild(text);
+    let text = document.createTextNode(item);
+
     section.appendChild(par);
-    article.appendChild(section); 
-       
+    par.appendChild(text);
+
+
+    let randomColor = randomColorLearners;
+    section.style.backgroundColor = randomColor();
+
 }
-createParagraphForLearners();
+
+ //If the background is dark the text should be white, if the background is light the text should be black
+
+    const colorPicker = () => {
+    let red = rand(225);
+    let green = rand(225);
+    let blue = rand(225);
+    let brightness  =  Math.sqrt((.299 * red * red) + (.587 * green * green) + (.114 * blue * blue));
+    return {"color": `rgb(${red}, ${green}, ${blue})`, "brightness": brightness}
+}      
+    
+ // Find a way so that everytime you load the page the order of the elements changes!
+     
+let shuffleLearners = (nameOfLearners) => {
+    for (i = nameOfLearners.length - 1; i >= 0; i--){
+        let randomNumber = Math.floor(Math.random() * (i + 1));
+        let randomItem = nameOfLearners[randomNumber];
+
+        nameOfLearners[randomNumber] = nameOfLearners[i];
+        nameOfLearners[i] = randomItem;
+    }
+  }
+shuffleLearners(nameOfLearners);
