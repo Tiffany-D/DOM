7b71e2f3ae10aaa32008e0bbcc585ca6107dd3c3
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-10-05 15:56:07 +0200

    exo 5 in progress

--- Events/script.js
+++ Events/script.js
@@ -1,15 +1,31 @@
 const _initTime = Date.now()
+const section = document.querySelector(".displayedsquare-wrapper");
 
-function getElapsedTime(){
-  return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+  
+function getElapsedTime() {
+ return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
-function clickOnSquare(e){
+
+
+function clickOnSquare(e) {
+  //create div
+  const displaySquares = document.createElement('div');
+  displaySquares.classList.add("displayedsquare");
+  displaySquares.classList.add(e.target.classList[1]);
+  section.append(displaySquares);
+  //create new li
+  const ul = document.querySelector("ul");
+  const newLi = document.createElement("li");
+  newLi.textContent = `[${getElapsedTime()}] Created a ${e.target.classList[1]} square`;
+  ul.append(newLi);
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
+  
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
 for(let actionsquare of actionsquares){
   actionsquare.addEventListener('click', clickOnSquare)
 }
+
